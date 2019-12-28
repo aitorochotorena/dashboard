823a889b4d85ac7701ca51f0f1672a98bcfef288
Tim Paine <t.paine154@gmail.com>
2019-12-27 19:21:36 -0500

    workingon #13

--- setup.py
+++ setup.py
@@ -1,36 +1,45 @@
 from setuptools import setup, find_packages
 from codecs import open
-from os import path
+import io
+import os
+import os.path
 
-here = path.abspath(path.dirname(__file__))
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+name = 'workspaces'
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+
+def get_version(file, name='__version__'):
+    path = os.path.realpath(file)
+    version_ns = {}
+    with io.open(path, encoding="utf8") as f:
+        exec(f.read(), {}, version_ns)
+    return version_ns[name]
+
+version = get_version(pjoin(here, 'dashboard', '_version.py'))
+
+with open(pjoin(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open(pjoin(here, 'requirements.txt'), encoding='utf-8') as f:
     requires = f.read().split()
 
 setup(
-    name='workspaces',
-    version='0.0.3',
+    name=name,
+    version=version,
     description='Dashboards',
     long_description=long_description,
     url='https://github.com/timkpaine/dashboard',
-    download_url='https://github.com/timkpaine/dashboard/archive/v0.0.3.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='Apache 2.0',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     keywords='dashboards',
